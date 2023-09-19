a764df10b2757fa1197aa168b158fcaeea20109e
Davi Bartilotti <bartilottidavi@gmail.com>
2023-09-19 10:32:52 -0300

    Início do Projeto

---
+++ e-commerce/src/App.jsx
@@ -0,0 +1,19 @@
+import NavBar from './components/NavBar.jsx';
+import CardProdutos from './components/CardProdutos.jsx'
+import './App.css';
+
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <div>
+        <h1>Página Inicial</h1>
+      </div>
+      <CardProdutos />
+    </>
+  )
+}
+
+export default App; 
---
+++ e-commerce/src/components/Produtos.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const produtos = {
+        produto1: {
+            nomeProduto: "Televisão Samsung 70' Ultra HD 4k",
+            categoria: "Eletrodomésticos",
+            marca: "Samsung",
+            valor: "R$6499,99"
+        }
+}
